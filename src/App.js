3eae4832c052f25bd862731b02d7bdb889181053
Janakiraman NPR <nprjanakiraman1987@gmail.com>
2018-01-29 18:05:10 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import web3 from './web3';
+import lottery from './lottery';
+
+class App extends Component {
+
+  state = {
+    manager: '',
+    players: [],
+    balance: ''
+  };
+
+  async componentDidMount(){
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+    this.setState({manager,players,balance});
+  }
+  render() {
+    return (
+      <div>
+      <h2>Lottery contract</h2>
+      <p>this contract is managed by {this.state.manager}</p>
+      There are currently {this.state.players.length} people entered competing to
+      win {web3.utils.fromWei(this.state.balance, 'ether')} ether.
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/web3.js
@@ -0,0 +1,5 @@
+import Web3 from 'web3';
+
+//uses provider used by metamask !!
+const web3 = new Web3(window.web3.currentProvider);
+export default web3;
